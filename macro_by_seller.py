ddb35da7cb5bfd762aa600e693fcfec5e2bc18ff
Seoyule Jo <seoyule@gmail.com>
2022-07-30 16:37:09 +0900

    두번째

--- macro_by_seller.py
+++ macro_by_seller.py
@@ -16,12 +16,12 @@ from PIL import Image
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import back_data
 
 fakes = ['디옷','디올','디오르','샤x','에르','에르메스','샤','구구','GC','CHA','루이','베네']
 ################################여기 입력해 주기###################################
 
-urls = ['https://sinsangmarket.kr/store/1477?isPublic=1',
-        'https://sinsangmarket.kr/store/21952?isPublic=1'
+urls = ['https://sinsangmarket.kr/store/21952?isPublic=1'
         ]
 jjim_order = "yes" #No : 최신순, Yes: 찜
 for_man = "no" #남자옷이면 "yes"
@@ -100,70 +100,7 @@ driver.find_element_by_xpath('//*[@id="frm_user"]/div/div[3]/button').click()
 print("cafe24 진입")
 
 # 분류작업
-category_list = {
-    '신상품': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[1]'],
-    '원피스': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[2]'],
-    '셔츠/남방': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[3]',
-              '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[1]'],
-    '블라우스': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[3]',
-             '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[2]'],
-    '니트': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[3]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[3]'],
-    '티&탑': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[3]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[4]'],
-    '빅사이즈': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[3]',
-             '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[5]'],
-    '임부복': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[3]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[6]'],
-    '스커트': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[4]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[1]'],
-    '청바지': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[4]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[2]'],
-    '팬츠': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[4]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[3]'],
-    '아우터': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[5]'],
-    '세트 아이템': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[6]'],
-    '샌들/슬리퍼/쪼리': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-                  '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[1]'],
-    '플랫/로퍼': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-              '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[2]'],
-    '힐/펌프스': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-              '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[3]'],
-    '웨지힐': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[4]'],
-    '스니커즈/운동화': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-                 '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[5]'],
-    '워커/부츠': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-              '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[6]'],
-    '수제화': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[7]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[7]'],
-    '쥬얼리': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[1]'],
-    '벨트': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[2]'],
-    '헤어핀/밴드': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-               '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[3]'],
-    '모자': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[4]'],
-    '선그라스/안경': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-                '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[5]'],
-    '시계': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[6]'],
-    '기타': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[8]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[7]'],
-    '가죽': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[9]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[1]'],
-    '숄더백': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[9]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[2]'],
-    '토트백': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[9]',
-            '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[3]'],
-    '크로스백': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[9]',
-             '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[4]'],
-    '클러치/지갑': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[9]',
-               '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[5]'],
-    '백팩': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[9]',
-           '//*[@id="eCategoryTbody"]/tr/td[2]/div/ul/li[6]'],
-    '남성의류': ['//*[@id="eCategoryTbody"]/tr/td[1]/div/ul/li[10]']}
+category_list = back_data.category_list
 
 driver.switch_to.window(driver.window_handles[0])
 
@@ -533,14 +470,7 @@ for k in range(len(urls)):
             driver.switch_to.window(driver.window_handles[0])
             action.send_keys(Keys.ESCAPE).perform() # 찜목록으로 재진입
             pyautogui.press('ctrl') # sleep 방지
-
-            #이미지 폴더 삭제
-            try:
-                shutil.rmtree(down_path+f"{j}_{subject}")
-            except OSError as e:
-                print("Error: %s : %s" % (down_path+f"{j}_{subject}", e.strerror))
-            print(k,"-",j, "번째아이템 완료")
-
+            print(k, "-", j, "번째아이템 완료")
         except:
             print(k,"-",j, "번째아이템 오류")
             error_.append(j-1)#index로 표시
@@ -552,6 +482,13 @@ for k in range(len(urls)):
             driver.switch_to.window(driver.window_handles[0])
             action.send_keys(Keys.ESCAPE).perform()  # 찜목록으로 재진입
             continue
+
+        finally:
+            # 이미지 폴더 삭제
+            try:
+                shutil.rmtree(down_path + f"{j}_{subject}")
+            except OSError as e:
+                print("Error: %s : %s" % (down_path + f"{j}_{subject}", e.strerror))
 
     error.append(error_)
 
